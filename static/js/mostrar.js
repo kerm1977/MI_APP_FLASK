99b9276f01aaa14d74c8850d9c4c470313b758b9
PLANTILLA <kenth1977@gmail.com>
2025-04-11 09:21:32 -0600

    oculta las letras en campos donde solo puende ir número en registro y contactos

--- static/js/mostrar.js
+++ static/js/mostrar.js
@@ -2,3 +2,8 @@ function limpiarNumero(input) {
   // Elimina cualquier carácter que no sea un número
   input.value = input.value.replace(/[^0-9]/g, '');
 }
+
+function evitarNumeros(input) {
+  // Elimina cualquier carácter numérico
+  input.value = input.value.replace(/[0-9]/g, '');
+}
